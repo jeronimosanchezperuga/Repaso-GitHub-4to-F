7295130286a75ea599ce8e6860fbf01ef9dc187f
jeronimosanchezperuga <70541244+jeronimosanchezperuga@users.noreply.github.com>
2022-04-05 10:46:39 -0300

    Exercise 5 from the Guia de Prog 1 completed

---
+++ Assets/Ej05.cs
@@ -0,0 +1,26 @@
+﻿//Realizá un programa que, dados dos números enteros num1 y num2 cargados desde el
+//Inspector muestre el resultado de la división del primero por el segundo en la consola
+//siguiendo el formato num1 + “ / “ + num2 + “ = “ + resultado.
+//En el caso de que el segundo valor num2 sea cero, mostrar el siguiente mensaje de error:
+//“No se puede dividir por cero”.
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ej05 : MonoBehaviour
+{
+    public int num1;
+    public int num2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(num1 + " / " +num2 + " = " + num1/num2);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
